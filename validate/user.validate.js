019becc9d73e22304f2fed07b2af561b32bdf521
LeDucThuan <dthuanle.20@gmail.com>
2019-07-31 22:25:16 +0700

    add validate

---
+++ validate/user.validate.js
@@ -0,0 +1,18 @@
+module.exports.postCreate = function(req, res, next){
+	var errors = [];
+	
+	if(!req.body.name){ errors.push('Name is required'); }
+	if(!req.body.age){ errors.push('Age is required'); }	
+	if(!req.body.phone){ errors.push('Phone is required'); }	
+	if(!req.body.email){ errors.push('Email is required'); }	
+	if(!req.body.password){ errors.push('Password is required'); }
+	
+	if(errors.length > 0){
+		res.render('users/create',{
+			errors: errors,
+			values: req.body
+		});
+		return;
+	}
+	next();
+};
